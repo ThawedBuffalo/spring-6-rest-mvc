e5c257435e9a54cd7a73d6b95db7cbd4008baa7c
JC <thawedbuffalo@gmail.com>
2025-06-06 08:33:48 -0400

    section 53 end

--- src/main/java/tbs/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/tbs/spring6restmvc/service/BeerServiceImpl.java
@@ -1,12 +1,25 @@
 package tbs.spring6restmvc.service;
 
 import tbs.spring6restmvc.model.Beer;
+import tbs.spring6restmvc.model.BeerStyle;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
-        return Beer.builder().id(id).build();
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Sabre Juice")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123456")
+                .price(new BigDecimal(12.99))
+                .quantityOnHand(25)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
     }
 }
