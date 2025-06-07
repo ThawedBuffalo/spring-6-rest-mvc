ed7a1b71ea56eb867b829572b587586db9ce1c2d
JC <thawedbuffalo@gmail.com>
2025-06-07 08:53:59 -0400

    section 61 end

--- src/main/java/tbs/spring6restmvc/controller/BeerController.java
+++ src/main/java/tbs/spring6restmvc/controller/BeerController.java
@@ -3,7 +3,9 @@ package tbs.spring6restmvc.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tbs.spring6restmvc.model.Beer;
 import tbs.spring6restmvc.services.BeerService;
@@ -14,15 +16,17 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method= RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "{beerId}", method =  RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
         log.debug("Get Beer by ID- in controller...");
         return beerService.getBeerById(id);
     }
