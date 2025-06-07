27122084f96321b0f4c565efdff90d503d0092ec
JC <thawedbuffalo@gmail.com>
2025-06-07 07:36:33 -0400

    section 55 end

--- src/main/java/tbs/spring6restmvc/controller/BeerController.java
+++ src/main/java/tbs/spring6restmvc/controller/BeerController.java
@@ -1,13 +1,22 @@
 package tbs.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import tbs.spring6restmvc.model.BeerStyle;
+import tbs.spring6restmvc.model.Beer;
+import tbs.spring6restmvc.services.BeerService;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
-    private final BeerStyle beerStyle;
+    private final BeerService beerService;
 
+    public Beer getBeerById(UUID id) {
+        log.debug("Get Beer by ID- in controller...");
+        return beerService.getBeerById(id);
+    }
 
 }
--- src/main/java/tbs/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/tbs/spring6restmvc/services/BeerServiceImpl.java
@@ -1,5 +1,7 @@
 package tbs.spring6restmvc.services;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import tbs.spring6restmvc.model.Beer;
 import tbs.spring6restmvc.model.BeerStyle;
 
@@ -7,9 +9,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer ID - in service. ID: " + id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
