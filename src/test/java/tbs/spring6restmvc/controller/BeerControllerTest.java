97060e0a130b905b016beaaa51293b4826c83c54
JC <thawedbuffalo@gmail.com>
2025-06-09 07:34:45 -0400

    section 77 complete

--- src/test/java/tbs/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/tbs/spring6restmvc/controller/BeerControllerTest.java
@@ -3,6 +3,7 @@ package tbs.spring6restmvc.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -14,6 +15,8 @@ import tbs.spring6restmvc.services.BeerServiceImpl;
 
 import java.util.UUID;
 
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -35,6 +38,19 @@ class BeerControllerTest {
     BeerService beerService;
 
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testDeleteBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        mockMvc.perform(delete("/api/v1/beer/" + beer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        ArgumentCaptor<UUID> uuidArgCaptor = ArgumentCaptor.forClass(UUID.class);
+        verify(beerService).deleteBeerById(uuidArgCaptor.capture());
+        assertThat(beer.getId()).isEqualTo(uuidArgCaptor.getValue());
+    }
 
     @Test
     void testUpdateBeer() throws Exception {
