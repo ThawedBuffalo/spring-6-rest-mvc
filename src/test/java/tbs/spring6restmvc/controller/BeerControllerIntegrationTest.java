9b086f6fb179723e8fdfe97740c6ca55b2fa659e
JC <thawedbuffalo@gmail.com>
2025-06-12 07:09:24 -0400

    section 102 complete

--- src/test/java/tbs/spring6restmvc/controller/BeerControllerIntegrationTest.java
+++ src/test/java/tbs/spring6restmvc/controller/BeerControllerIntegrationTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import tbs.spring6restmvc.entities.Beer;
 import tbs.spring6restmvc.model.BeerDTO;
 import tbs.spring6restmvc.repositories.BeerRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +22,20 @@ class BeerControllerIntegrationTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerById() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+        assertThat(dto.getId()).isEqualTo(beer.getId());
+    }
+
+    @Test
+    void testGetBeerByIdFailed() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
 
     @Test
     void testListBeers() {
