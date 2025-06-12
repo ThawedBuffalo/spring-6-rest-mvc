e4b2d7ed83013f5e298ee377d500c126b5cd6ceb
JC <thawedbuffalo@gmail.com>
2025-06-12 07:00:51 -0400

    section 101 complete

---
+++ src/test/java/tbs/spring6restmvc/controller/BeerControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package tbs.spring6restmvc.controller;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import tbs.spring6restmvc.model.BeerDTO;
+import tbs.spring6restmvc.repositories.BeerRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIntegrationTest {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(3);
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    void testEmptyListBeers() {
+        beerRepository.deleteAll();
+        List<BeerDTO> beers = beerController.listBeers();
+        assertThat(beers.size()).isEqualTo(0);
+    }
+}
